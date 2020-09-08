e7048ab8a72b4ab65307d118c1bc46b9e3ce407a
Niloy009 <niloy.roy@northsouth.edu>
2020-09-07 18:23:28 -0700

    total tweet page design done

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,13 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img class="rounded-full mr-2" src="https://picsum.photos/50/50" alt="images">
+    </div>
+    <div>
+        <h5 class="font-bold mb-4">
+            John Doe
+        </h5>
+        <p class="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid asperiores aspernatur doloribus ea illo in itaque maiores molestiae, nam officia omnis provident quia reiciendis repellendus sunt velit! Libero, magni.
+        </p>
+    </div>
+</div>
